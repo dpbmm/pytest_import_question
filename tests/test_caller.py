35f9127dccceda17686fe68660f2a6edaa2e1c2c
David Branner <dpb.mediamath@gmail.com>
2015-07-24 14:23:19 -0400

    comment out `sys` imports; remove extraneous lines

--- tests/test_caller.py
+++ tests/test_caller.py
@@ -1,10 +1,8 @@
-import sys
-sys.path.extend('lib')
+#import sys
+#sys.path.extend('lib')
 #sys.path.extend(['..', 'lib'])
 import lib.create
 
 def test_show():
-#    result = lib.create.create()
-#    print 'result:', result
     assert True
 
